3fd93d213097ea02dadc4d5570bdcea5282bcdb0
bezoda01 <86252841+bezoda01@users.noreply.github.com>
2022-01-14 22:59:24 +0300

    complete firstTest

--- src/test/java/PartOne.java
+++ src/test/java/PartOne.java
@@ -1,7 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class PartOne{
     private WebDriver driver;
@@ -9,10 +13,24 @@ public class PartOne{
 
     @BeforeTest
     void setUp() {
+        WebDriverManager.chromedriver().setup();
         firstPage = new FirstPage(driver);
     }
 
+    @Test
+    void test() {
+        firstPage.goToPage();
+        Assert.assertEquals(firstPage.clickToAbout().getText(), "О STEAM", "Page was not open");
+        firstPage.clickToAbout().click();
+        Assert.assertTrue(firstPage.onlineComparison(), "The number of players is now more than online");
+        firstPage.clickToMarket().click();
+        Assert.assertEquals(firstPage.isThatMarket().getText(),"Ваш магазин");
+    }
 
+    @AfterTest
+    void quit() {
+        firstPage.quit();
+    }
 
 
 }
